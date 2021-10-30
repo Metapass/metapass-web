00785b29280771d64893d596af7982ecc9dbd791
Daksh Kulshreshtha <akshd@outlook.com>
2021-10-30 23:35:12 +0530

    initialized chakra & purged unwanted css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import { mode } from "@chakra-ui/theme-tools";
+
+
+const theme = extendTheme({
+    fonts: {
+        heading: "Inter",
+        body: "Inter",
+    },
+    config: {
+        initialColorMode: "dark",
+        useSystemColorMode: false,
+    },
+    styles: {
+        global: (props: any) => ({
+            body: {
+                color: mode("gray.800", "whiteAlpha.900")(props),
+                bg: mode("gray.100", "#141214")(props),
+            },
+        }),
+    },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
 
+    return (
+        <ChakraProvider theme={theme}>
+   			<Component {...pageProps} />
+    	</ChakraProvider>
+    )
+}
 export default MyApp
