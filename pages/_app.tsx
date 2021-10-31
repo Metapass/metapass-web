b264a60071f2c8f4d792b85f78d93647ee9fca44
anoushk1234 <anoushka1828@gmail.com>
2021-10-31 12:22:15 +0530

    theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,10 @@
 import type { AppProps } from 'next/app'
-import { ChakraProvider, extendTheme } from "@chakra-ui/react"
-import { mode } from "@chakra-ui/theme-tools";
+import { ChakraProvider } from "@chakra-ui/react"
+import theme from "../utils/theme"
 import "@fontsource/inter"
 import Layout from '../components/Layout'
 
-const theme = extendTheme({
-    fonts: {
-        heading: "Inter",
-        body: "Inter",
-    },
-    config: {
-        initialColorMode: "dark",
-        useSystemColorMode: false,
-    },
-    styles: {
-        global: (props: any) => ({
-            body: {
-                color: mode("gray.800", "whiteAlpha.900")(props),
-                bg: mode("gray.100", "#141214")(props),
-            },
-        }),
-    },
-});
+
 
 function MyApp({ Component, pageProps }: AppProps) {
 
